c944ded54c8837800ad62c34f37f374dea24e32c
dzhelyazkov <d.zhelyazkov@ocado.com>
2019-11-10 19:53:04 +0200

    Image tests introduced
    Image get longevity test introduced
    Script for removal of redundant image files introduced

---
+++ src/integration-test/java/com/xrc/camera/Constants.java
@@ -0,0 +1,10 @@
+package com.xrc.camera;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public final class Constants {
+
+    public static final DateFormat IMAGE_FILE_DATE_FORMAT =
+            new SimpleDateFormat("HH-mm-ss");
+}
---
+++ src/integration-test/java/com/xrc/camera/ImageVerifier.java
@@ -0,0 +1,21 @@
+package com.xrc.camera;
+
+import com.xrc.awt.Dimensions;
+import org.junit.Assert;
+
+import java.awt.Dimension;
+import java.awt.image.RenderedImage;
+
+public class ImageVerifier {
+
+    private static final Dimension NULL_DIMENSION = new Dimension(0, 0);
+
+    public void verify(RenderedImage image) {
+        Assert.assertNotNull(image);
+
+        Dimension imageDimension = new Dimension(image.getWidth(), image.getHeight());
+        Assert.assertTrue("Invalid image dimension: " + imageDimension,
+                Dimensions.compare(imageDimension, NULL_DIMENSION) > 0
+        );
+    }
+}
